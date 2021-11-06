5e95bfcd1642e51829d4e37b9e7587c0f31495f0
marnixah <marnix@hagehoeve.com>
2021-11-07 00:07:15 +0100

    feat: get direction between 2 positions

--- lib/funcs.lua
+++ lib/funcs.lua
@@ -54,3 +54,45 @@ function table.has_value (tab, val)
 
   return false
 end
+
+-- A function that returns the facing direction between 2 points
+function getDirection(x1, y1, z1, x2, y2, z2)
+  local xDiff = x2 - x1
+  local yDiff = y2 - y1
+  local zDiff = z2 - z1
+  if xDiff == 0 and yDiff == 0 and zDiff == 0 then
+    return "SELF"
+  elseif xDiff == 0 and yDiff == 0 then
+    if zDiff > 0 then
+      return "NORTH"
+    else
+      return "SOUTH"
+    end
+  elseif xDiff == 0 and zDiff == 0 then
+    if yDiff > 0 then
+      return "UP"
+    else
+      return "DOWN"
+    end
+  elseif yDiff == 0 and zDiff == 0 then
+    if xDiff > 0 then
+      return "EAST"
+    else
+      return "WEST"
+    end
+  else
+    local angle = math.atan2(yDiff, xDiff)
+    if angle < 0 then
+      angle = angle + 2 * math.pi
+    end
+    if angle < math.pi / 4 then
+      return "EAST"
+    elseif angle < math.pi / 2 then
+      return "SOUTH"
+    elseif angle < 3 * math.pi / 4 then
+      return "WEST"
+    else
+      return "NORTH"
+    end
+  end
+end
