a42d2111074b000d93c257a4038174a18f8fe84e
marnixah <marnix@hagehoeve.com>
2021-11-07 00:09:04 +0100

    feat: import fhttp and homeassistant on startup

--- startup/05_load_apis.lua
+++ startup/05_load_apis.lua
@@ -1,5 +1,7 @@
 shell.setPath(shell.path()..":/bin")
 dofile('/lib/env.lua')
 dofile('/lib/funcs.lua')
+dofile('/lib/fhttp.lua')
 dofile('/lib/bezosmapsapi.lua')
+dofile('/lib/homeassistantapi.lua')
 dofile('/lib/turtleapi.lua')
