416e6e930c890d78a3a1b58a4a2ee71b7b5cdc6a
marnixah <marnix@hagehoeve.com>
2021-11-07 00:09:53 +0100

    feat: promise support
    Totally written by me

---
+++ lib/promise.lua
@@ -0,0 +1,72 @@
+Promise = {}
+
+function Promise.isPromise(obj)
+  if obj == nil then
+    return false
+  end
+  return obj.after ~= nil
+end
+
+-- Pure magic written by copilot
+function Promise.new(fn)
+  local self = setmetatable({}, {__index = Promise})
+  self.state = 'pending'
+  self.value = nil
+  self.handlers = {}
+  self.resolved = false
+  self.rejected = false
+  self.resolve = function(value)
+    if self.resolved or self.rejected then return end
+    self.resolved = true
+    self.value = value
+    self.state = 'resolved'
+    for _, handler in ipairs(self.handlers) do
+      handler(value)
+    end
+  end
+  self.reject = function(reason)
+    if self.resolved or self.rejected then return end
+    self.rejected = true
+    self.value = reason
+    self.state = 'rejected'
+    for _, handler in ipairs(self.handlers) do
+      handler(reason)
+    end
+  end
+  self.after = function(onFulfilled, onRejected)
+    local nextPromise = Promise.new()
+    table.insert(self.handlers, function(value)
+      if self.state == 'resolved' then
+        if type(onFulfilled) == 'function' then
+          local result = onFulfilled(value)
+          if Promise.isPromise(result) then
+            result:after(nextPromise.resolve, nextPromise.reject)
+          else
+            nextPromise.resolve(result)
+          end
+        else
+          nextPromise.resolve(value)
+        end
+      elseif self.state == 'rejected' then
+        if type(onRejected) == 'function' then
+          local result = onRejected(value)
+          if Promise.isPromise(result) then
+            result:after(nextPromise.resolve, nextPromise.reject)
+          else
+            nextPromise.resolve(result)
+          end
+        else
+          nextPromise.reject(value)
+        end
+      end
+    end)
+    return nextPromise
+  end
+  if type(fn) == 'function' then
+    local loop = libfredio.get_event_loop()
+    loop.task(libfredio.async(fn)(self.resolve, self.reject))
+  else
+    self.resolve(fn)
+  end
+  return self
+end
--- startup/05_load_apis.lua
+++ startup/05_load_apis.lua
@@ -1,6 +1,7 @@
 shell.setPath(shell.path()..":/bin")
 dofile('/lib/env.lua')
 dofile('/lib/funcs.lua')
+dofile('/lib/promise.lua')
 dofile('/lib/fhttp.lua')
 dofile('/lib/bezosmapsapi.lua')
 dofile('/lib/homeassistantapi.lua')
