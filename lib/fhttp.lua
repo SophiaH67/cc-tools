de41496f5e24720b1db1cb0c698d3cd2231ccc66
marnixah <marnix@hagehoeve.com>
2021-11-07 02:07:17 +0100

    feat: make fhttp use promises

--- lib/fhttp.lua
+++ lib/fhttp.lua
@@ -2,17 +2,32 @@ local client = libfrednet.RTPClient("1.0.1", 80)
 
 fhttp = {}
 
+
 function fhttp.init()
   local loop = libfredio.get_event_loop() 
   loop.task(libfrednet.connect())
 end
 
 function fhttp.get(url, headers)
-  local res = client.get_resource("/get", {url=url, headers=headers})
-  return res.status_code, res.body
+  return Promise.new(function(resolve, reject)
+    local res = client.get_resource("/get", {url=url, headers=headers})
+    -- if res.status_code is between 200 and 400
+    if res.status_code >= 200 and res.status_code < 400 then
+      resolve(res)
+    else
+      reject(res)
+    end
+  end)
 end
 
-function fhttp.post(url, headers, data)
-  local res = client.get_resource("/post", {url=url, headers=headers, data=data})
-  return res.status_code, res.body
+function fhttp.post(url, body, headers)
+  return Promise.new(function(resolve, reject)
+    local res = client.get_resource("/post", {url=url, headers=headers, body=body})
+    -- if res.status_code is between 200 and 400
+    if res.status_code >= 200 and res.status_code < 400 then
+      resolve(res)
+    else
+      reject(res)
+    end
+  end)
 end
