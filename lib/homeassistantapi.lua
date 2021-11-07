ee500082c23cff44dd42416a2027cb72bc265eb6
marnixah <marnix@hagehoeve.com>
2021-11-07 02:07:33 +0100

    fix: homeassistant compatible with fhttp promises

--- lib/homeassistantapi.lua
+++ lib/homeassistantapi.lua
@@ -2,44 +2,35 @@ homeassistant = {}
 
 function homeassistant.get_state(entity_id)
   local url = env.homeassistant_base .. "/api/states/" .. entity_id
-  local response = http.get(url, {
+  local response = fhttp.get(url, {
     ["Authorization"] = "Bearer " .. env.homeassistant_token
   })
-  if response then
-    print(response.readAll())
-    response.close()
-  end
+  response.after(function() print("OK!") end, function() print("FAIL!") end)
   return nil
 end
 
 function homeassistant.turnOnLight(entity_id)
   local url = env.homeassistant_base .. "/api/services/light/turn_on"
-  local response = http.post(url, textutils.serialiseJSON({
+  local response = fhttp.post(url, textutils.serialiseJSON({
     ["entity_id"] = entity_id
   }),{
     ["Authorization"] = "Bearer " .. env.homeassistant_token,
     ["Content-Type"] = "application/json",
     ["Accept"] = "application/json",
   })
-  if response then
-    print(response.readAll())
-    response.close()
-  end
+  response.after(function() print("OK!") end, function() print("FAIL!") end)
   return nil
 end
 
 function homeassistant.turnOffLight(entity_id)
   local url = env.homeassistant_base .. "/api/services/light/turn_off"
-  local response = http.post(url, textutils.serialiseJSON({
+  local response = fhttp.post(url, textutils.serialiseJSON({
     ["entity_id"] = entity_id
   }),{
     ["Authorization"] = "Bearer " .. env.homeassistant_token,
     ["Content-Type"] = "application/json",
     ["Accept"] = "application/json",
   })
-  if response then
-    print(response.readAll())
-    response.close()
-  end
+  response.after(function() print("OK!") end, function() print("FAIL!") end)
   return nil
 end
